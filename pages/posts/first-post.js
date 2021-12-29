5a0523c614b9a47463a1e54c6b9c95a6d117160b
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-12-30 01:53:40 +0530

    changed tittle and added Script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,22 @@
+import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FistPost(){
     return( 
     <>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        {/* strategy controls when the third-party script should load. A value of lazyOnload tells Next.js to load this particular script lazily during browser idle time */}
+        {/* onLoad is used to run any JavaScript code immediately after the script has finished loading. In this example, we log a message to the console that mentions that the script has loaded correctly */}
+        <Script
+            src=""
+            strategy="lazyOnload"
+            onLoad={()=>
+                console.log('Script loaded correctly')
+            }
+        />
         <h1>First Post</h1>
         <h2>
             <Link href='/'>
